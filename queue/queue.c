c1ee52322d172c6f4a9b4c9d59eb3e67533a25f1
Surya N <suryanatarajan04@gmail.com>
2024-09-10 18:55:19 +0530

    Completed queue using C

--- queue/queue.c
+++ queue/queue.c
@@ -1,90 +1,85 @@
 #include <stdio.h>
 #include<conio.h>
+int front=-1,rear=-1,choice=0;
 void insert();
 void delete();
 void display();
 void isEmptyOrNot();
-
-int front=-1,rear=-1,choice=0;
 void main()
 {
-    int size=0;
-    printf("\t\tQueue using array\n\n");
+    int size;
+    printf("\t\t\033[1;31mQueue using array\033[1m\n\n\n");
     printf("Enter the size of the Queue :");
     scanf("%d",&size);
     int queue[size]; //declared queue and assigned the size
+   
     while(choice!=5){
-        printf("\n1.Insertion\n2.Deletion\n3.Display\n4.isEmptyOrNot\n5.Exit\n");
-        printf("\nChoose one to perform : ");
+        printf("\n\033[1;33m1.Insertion\n2.Deletion\n3.Display\n4.isEmptyOrNot\n5.Exit\033[0m\n");
+        printf("\n\033[1;32mChoose one to perform : \033[0m");
         scanf("%d",&choice);
-        while(choice!=5){
             switch(choice){
-                case 1:{
-                insert(queue);
+                case 1:
+                insert(queue,size);
                 break;
-                }
-                case 2:{
+                case 2:
                 delete(queue,size);
                 break;
-                }
-                case 3:{
+                case 3:
                 display(queue,size);
                 break;
-                }
-                case 4:{
-                isEmptyOrNot(queue,size);
+                case 4:
+                isEmptyOrNot(size);
                 break;
-                }
-                case 5:{
+                case 5:
+                printf("\nProgram Terminated\n");
                 break;
-                }
                 default:
-                printf("\nEnter correct choice.");
+                printf("\nEnter correct choice.\n\n");
             }//end of switch
-        } //end of inner while
     }//end of outer while
     getch();
 }
 
-void insert(int queue[],int size,int rear){
-    int val=0;
-    if(rear==size){ 
-        printf("\nqueue full, cannot insert values.\n"); 
+void insert(int queue[],int size){
+    int val;
+    if(rear==size-1){
+        printf("\nqueue full, cannot insert values.\n");
     }
     else{
     printf("\nEnter the value to be insert into the queue : ");
     scanf("%d",&val);
     rear++;
     queue[rear] = val;
+    front=0;
     }    
 }
 
-void delete(int queue[],int front){
-    if(front==-1){ 
-        printf("\nNo values to delete.\n"); 
-        
+void delete(int queue[],int size){
+    if(front==size){
+        printf("\nNo values to delete.\n");
     }
     else{
-        printf("%d deleted from the queue.\n",queue[rear]);
+        printf("\n%d deleted from the queue.\n",queue[front]);
         front++;
+        rear--;
     }
 }
 
-void display(int queue[],int size,int front){
-    int i=0;
-    if(rear>-1){
-        for(i=front;i<=size;i++){
+void display(int queue[],int size){
+    int i;
+    if(rear==-1){
+        printf("\nNo values to display\n");
+    }else{
+        for(i=front;i<=rear;i++){
             printf("%d -> %d\n",i,queue[i]);
         }
     }
-    else{ printf("\nNo values to display\n");}
-    
+   
 }
-void isEmptyOrNot(int size,int front,int rear){
-    int res = size-rear;
-    if(rear!=size){
-         printf("\nQueue Not Empty. %d space available \n",res);
+void isEmptyOrNot(int size){
+    if(rear>-1){
+         printf("\nQueue Not Empty\n");
     }else{
-         printf("\nQueue full.\n");
+         printf("\nQueue Empty.\n");
     }
 }
