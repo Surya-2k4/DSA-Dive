a5850785ecf67d49b02f042f6070943da7206bac
Surya N <suryanatarajan04@gmail.com>
2024-09-21 16:15:02 +0530

    Update binarySearch.c

--- binarySearch/binarySearch.c
+++ binarySearch/binarySearch.c
@@ -1,13 +1,47 @@
 #include <stdio.h>
 
-void binarySearch();
+int binarySearch();
+void sort();
 
-void binarySearch(int arr[],int target,int size){
+int binarySearch(int arr[],int target,int size){
+    
+    sort(arr,size);//sort the array for performing binarySearch
+    
+    int left=0, right=size - 1;  
+    
+    while(left <= right){
+    int mid=left+(left+right)/2; // Avoid potential overflow
+    
+    if(target == mid){
+        return mid;  // Target found at index mid
+    }
+    else if(arr[mid] < target){
+        left = mid + 1;  // Search in the right half (left)
+    }
+    else{
+        right = mid -1;  //  Search in the left half (left)
+    }
+    }
+    return -1;
+}
 
-
-
-
-
+//for performing the binarySearch need to sort the array
+void sort(int arr[],int size){
+    
+    for(int i=0; i<size; i++){
+          for(int j=0; j<size-i;j++){
+              if(arr[j] > arr[j+1]){
+                  // swapping arr[j] - > arr[j+1]
+                  int temp = arr[j];
+                  arr[j] = arr[j+1];
+                  arr[j+1] = temp;
+              }
+          } 
+    }
+    // printf("\nAfter performing sort operation..");
+    // for(int i=0;i<size;i++){
+    //     printf("%d ",arr[i]);
+    // }
 }
 
 
@@ -17,14 +51,18 @@ void main()
     printf("Enter the size for the Array : ");
     int size;
     scanf("%d",&size);
-    printf("Enter the values separated by space : ");
+    printf("\nEnter the values separated by space : ");
     int arr[size],i,value,target;
     for(i = 0 ; i < size; i++){
         scanf("%d",&value);
         arr[i] = value;
     }
-    printf("Enter target value : ");
+    printf("\nEnter target value : ");
     scanf("%d",&target);
-
-    binarySearch(arr,target,size);
+    
+    int res = binarySearch(arr,target,size);
+    
+    
+    if(res>-1){ printf("\n%d found at %d index.",target,res); }
+    else{ printf("\n%d not found.",target); }
 }
